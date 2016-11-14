592a51b10e1bf21402517351bbfb0c1931da0613
luoli <877904742@qq.com>
2016-11-14 19:35:29 +0800

    my first commit

---
+++ hello.h
@@ -0,0 +1,13 @@
+//
+//  hello.h
+//  
+//
+//  Created by luolihacker on 16/8/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hello : UIView
+@property (nonatomic, strong) UIView *firstView;
+@end
